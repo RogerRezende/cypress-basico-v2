e7fddb89c381f26d196209e3182dcd8cc53410f7
roger.santos <roger.rezende@gmail.com>
2022-04-06 09:44:42 -0300

    teste para validar título da aplicação

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,9 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    this.beforeEach(() => cy.visit('../../src/index.html'))
+
+    it('Verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+})
