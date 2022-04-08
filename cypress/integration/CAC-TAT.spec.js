880d029c322b880b0fe5297dcc1c37ad7281ffa8
roger.santos <roger.rezende@gmail.com>
2022-04-08 08:27:16 -0300

    exercícios da aula 3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -162,4 +162,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success > strong')
             .should('be.visible')
     })
+
+    it('seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
 })
